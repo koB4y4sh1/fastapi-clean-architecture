85285e596ff78893926da65fa6c7d32f7b78c982
小林 研斗 <kbyknt01.pub@taiseigr.com>
2025-07-19 15:45:13 +0900

    :fire: ディレクトリ構成の変更

--- api/src/dependencies/user_dependencies.py
+++
@@ -1,9 +0,0 @@
-# src/di/container.py
-from src.application.user_service import UserService
-from src.adapter.out.user_repository_impl import InMemoryUserRepository
-from src.interface.user_interface_service import UserInterfaceService
-
-def get_user_interface_service() -> UserInterfaceService:
-    repo = InMemoryUserRepository()
-    service = UserService(repo=repo)
-    return UserInterfaceService(service=service)
